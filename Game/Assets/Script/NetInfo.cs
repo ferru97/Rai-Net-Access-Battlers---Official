9c947430da973abca766bcbbb80066396e432966
Yannodo <99420724+Yannodo@users.noreply.github.com>
2023-09-14 20:42:13 +0200

    Updated Unity version, cleaned GUI

--- Game/Assets/Script/NetInfo.cs
+++ Game/Assets/Script/NetInfo.cs
@@ -9,7 +9,7 @@ public class NetInfo
 
 
 
-    public static string WEBSITE_ADDR = "https://www.rainetdigital.com";
+    public static string WEBSITE_ADDR = "https://kirou.wordpress.com/resources/rai-net-access-battlers-user-manual-english-translation-v-111228/";
     public static string PAYPAL_ADDR = "http://www.paypal.com/cgi-bin/webscr?cmd=_s-xclick&hosted_button_id=QWDXLYTLAA6RL&source=url";
     public static string DISCORD_ADDR = "https://discord.gg/f22dhpu";
 
